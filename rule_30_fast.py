637055a7bf04cba4578bc7e8335c3b97ef419d8e
EL-S <31227083+EL-S@users.noreply.github.com>
2019-07-30 23:37:44 +1000

    Shorter and Faster
    a faster and shorter implementation

--- rule_30_fast.py
+++ rule_30_fast.py
@@ -1,16 +1,11 @@
 current_state = [0,1,0]
 
-def next_state(current_state):
-    current_state, new_state = [0] + current_state + [0], []
-    for i, cell in enumerate(current_state[1:-1]):
-        new_state += [current_state[i] ^ (cell | current_state[i+2])]
-    return [0] + new_state + [0]
-
 results = []
 
 while True:
     for i in range(100):
-        current_state = next_state(current_state)
+        current_state = [0] + list(map(lambda c,p,n:p^(c|n), current_state, [0] + current_state, current_state[1:] + [0])) + [0]
         results.extend([current_state[round(len(current_state)/2)]])
     print(results)
     results = []
+    
